c7819f2b20ca9fd2c11f9f3304267fc2d45d0000
Nathan Tucker <abezhat@verizon.net>
2016-11-29 20:53:57 -0800

    Removing jquery requirement

--- theme/assets/js/index.js
+++ theme/assets/js/index.js
@@ -1,14 +1,5 @@
-(function ($, undefined) {
-    "use strict";
-
-    $(document).ready(function () {
-        /* Keeping in there in case we need to every post videos */
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
-
-        //Set each list item to a random bottom border
-        $('.new-nav a').each( function() {
-            $( this ).css( 'border-bottom', '5px solid rgba(' + Math.floor( Math.random( ) * 255 ) + ',' + Math.floor( Math.random( ) * 255 ) + ',' + Math.floor( Math.random( ) * 255 ) + ',.8)' );
-        });
-    });
-})(jQuery);
+var all_navs = document.querySelectorAll( '.new-nav a' );
+ 
+for ( var i = 0; i < all_navs.length; i++ ) {
+    all_navs[i].style.borderBottom = '5px solid rgba(' + Math.floor( Math.random( ) * 255 ) + ',' + Math.floor( Math.random( ) * 255 ) + ',' + Math.floor( Math.random( ) * 255 ) + ',.8)';
+}

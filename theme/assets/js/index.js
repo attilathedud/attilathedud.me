3a6d0d6355addfcca39453ddee4304622e23c0ef
Nathan Tucker <abezhat@verizon.net>
2018-01-21 19:41:16 +0900

    Moving css stuff to sass, some css cleanup

--- theme/assets/js/index.js
+++ theme/assets/js/index.js
@@ -9,7 +9,7 @@ $(function () {
             if (page <= max_pages && !is_loading) {
                 is_loading = true;
                 $('.loading').fadeIn();
-
+                
                 $.get((url_blog + '/page/' + page), function (content) {
                     $('.content').append($(content).find(".post"));
                     page = page + 1;
@@ -20,7 +20,7 @@ $(function () {
         }
     });
 
-    $('.new-nav a').each(function () {
+    $('.nav-bar a').each(function () {
         $(this).css('border-bottom', '5px solid rgba(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',.8)');
     });
 });
